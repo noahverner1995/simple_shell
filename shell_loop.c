970a4e608e287cf3adf64a369129ec11772af9b6
Noah Verner <83596569+noahverner1995@users.noreply.github.com>
2021-12-07 15:48:00 -0500

    This is the shell_loop.c file
    This file has a function called shellLoop which is responsible for taking the commands passed by the user and use those as parameters in other functions which are called inside a while TRUE loop, which is the loop that will simulate the behaviour of the original Shell both in CLI look and execution.

---
+++ shell_loop.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * shellLoop - Run the shell "forever"
+ * @argv: command passed by the user
+ * Return: nothing
+ */
+void shellLoop(char **argv)
+{
+ssize_t getLine = 1;
+char **tokens = NULL, *buffer = NULL, *fullPath = NULL;
+char *promt = "($) ";
+Node *path = NULL;
+char *pathCopy = NULL;
+int errorShowed = 0, counter = 1;
+
+path = listpath(&pathCopy);
+while (TRUE)
+{
+write(1, promt, 5);
+fflush(stdout);
+getLine = readLine(&buffer, &tokens);
+if (getLine == EOF)
+break;
+fullPath = addPath(&tokens, path);
+isPath(&tokens, &fullPath, argv, &counter, &errorShowed);
+executeLine(&buffer, &tokens, fullPath);
+		
+counter++;
+}
+free_list(path);
+free(pathCopy);
+free(buffer);
+}
