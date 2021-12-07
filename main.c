3459649b271393dc782d612a1141fb937a9804de
Noah Verner <83596569+noahverner1995@users.noreply.github.com>
2021-12-07 15:39:02 -0500

    This is the main.c file
    This file is responsible for executing the function signal in case the user press Ctrl-c when using the interactive mode of the Simple Shell,  also, it passes de commands stored in the *argv[] to the main function called "shellLoop"

---
+++ main.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * controlC - write to the stdout "\n($) "
+ * @var: n
+ */
+void controlC(int var)
+{
+write(1, "\n($) ", 5); 
+(void) var; 
+}
+
+/**
+ * main - Execute shellLoop function with the
+ * output of controlC function passed as argv
+ * @argc: number of arguments
+ * @argv: characters of the argument
+ * Return: nothing
+ */
+int main(int argc, char **argv)                       
+{
+(void) argc;
+signal(SIGINT, controlC);
+shellLoop(argv);
+
+return (0);
+}
